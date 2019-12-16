f5dec0e1c9a5cd31e059fe1a226a648c911a8e62
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-16 10:37:25 -0500

    se agrego 13avo ejercicio

---
+++ ejercicio13.py
@@ -0,0 +1,17 @@
+#ejercicio interaccion                                                                                                                                                                                                                                                                                                                                            #ejercici interacci
+import os
+clave=os.sys.argv[3]
+for letra in clave:
+    print(letra)
+clave="#$I."
+for letra in clave:
+    if(letra=="#"):
+        print("carla")
+    if(letra=="$"):
+        print("cáceres")
+        print("###############")
+    if (letra=="%"):
+        print("Rojas")
+    #fin if
+#fin for
+print("#########################")
