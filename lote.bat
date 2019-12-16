6175c7880c9b10c0e37b7920fbe464795e987f63
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-16 11:18:38 -0500

    se agrego lote.bat

---
+++ lote.bat
@@ -0,0 +1,19 @@
+python ejercicio11.py "# $ I ."
+python ejercicio11.py "& / ( ,"
+python ejercicio11.py "a c r t"
+
+python ejercicio12.py "& # $ ="
+python ejercicio12.py "q w e r"
+python ejercicio12.py "# $ ! /"
+
+python ejercicio13.py  "# $ I ."
+python ejercicio13.py  "& / ( ,"
+python ejercicio13.py  "$ = % ,"
+
+python ejercicio14.py   "# & %"
+python ejercicio14.py   "$ % &"
+python ejercicio14.py   "% & $"
+
+python ejercicio15.py    "# % &"
+python ejercicio15.py    "$ % &"
+python ejercicio15.py    "& / ("
