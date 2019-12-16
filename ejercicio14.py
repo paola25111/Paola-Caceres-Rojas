a83f13c12bf06ec8bde5c9f92ac8d3e8fe9b2b78
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-16 10:37:53 -0500

    se agrego 14avo ejercicio

---
+++ ejercicio14.py
@@ -0,0 +1,18 @@
+#ejercicio interaccion
+import os
+compras=os.sys.argv[4]
+for letra in compras:
+    print(letra)
+compras="#&%"
+for letra in compras:
+    if(letra=="#"):
+        print("Arroz")
+        print("#############")
+    if(letra=="&"):
+        print("pollo")
+        print("###############")
+    if (letra=="%"):
+        print("papa")
+    #fin if
+#fin for
+print("#########################")
