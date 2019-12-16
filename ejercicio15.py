4197f4da75af30a90179d9ba6b2cd034832d9c69
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-16 10:38:23 -0500

    se agrego 15avo ejercicio

---
+++ ejercicio15.py
@@ -0,0 +1,18 @@
+#ejercicio interaccion
+import os
+frases=os.sys.argv[5]
+for letra in frases:
+    print(letra)
+frases="#%&"
+for letra in frases:
+    if(letra=="#"):
+        print("te")
+        print("#############")
+    if(letra=="%"):
+        print("quiero")
+        print("###############")
+    if (letra=="&"):
+        print("mucho")
+    #fin if
+#fin for
+print("###########################")
