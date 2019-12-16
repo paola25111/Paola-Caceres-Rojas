edaa700abc49919448f211e5c840e29bdcf7d600
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-16 10:28:44 -0500

    se agrego 6to ejercicio

---
+++ ejercicio06.py
@@ -0,0 +1,13 @@
+#ejercicio  para
+i=0
+s=0
+max=100
+while(i<=max):
+    i+=1
+    if((i%2)==0):
+       print(i)
+       s=s+i
+    #fin_if
+#fin_while
+print("termine prog")
+print("suma pares:",s)
